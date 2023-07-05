7e2b0e20473d76ba05ccc2832e5a65bf05bc8e57
MdTasib <ohidulalam442@gmail.com>
2023-07-05 21:18:11 +0600

    error deploy

--- dist/app.js
+++ dist/app.js
@@ -16,6 +16,9 @@ app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 // ROUTES
+app.get("/", (req, res) => {
+    res.send("Testing route!");
+});
 app.use("/api/v1/", routes_1.default);
 //Testing
 // app.get("/", async (req: Request, res: Response, next: NextFunction) => {
