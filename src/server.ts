4f14ca116333573b2540eaf722e2d9a5ac5c1439
MdTasib <ohidulalam442@gmail.com>
2023-07-10 18:51:38 +0600

    logger delete

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { errorLogger, logger } from "./shared/logger";
 import { Server } from "http";
 
 process.on("uncaughtException", error => {
-	errorLogger.error(error);
+	console.log(error);
 	process.exit(1);
 });
 
@@ -15,19 +15,19 @@ let server: Server;
 async function bootstrap() {
 	try {
 		await mongoose.connect(config.database_url as string);
-		logger.info(`🛢 Database connection successful`);
+		console.log(`🛢 Database connection successful`);
 
 		server = app.listen(config.port, () => {
-			logger.info(`Server is  listening on port ${config.port}`);
+			console.log(`Server is  listening on port ${config.port}`);
 		});
 	} catch (err) {
-		errorLogger.error(`Failed to connect database`, err);
+		console.log(`Failed to connect database`, err);
 	}
 
 	process.on("unhandleRejection", error => {
 		if (server) {
 			server.close(() => {
-				errorLogger.error(error);
+				console.log(error);
 				process.exit(1);
 			});
 		} else {
@@ -38,7 +38,7 @@ async function bootstrap() {
 bootstrap();
 
 process.on("SIGTERM", () => {
-	logger.info("SIGTERM is received");
+	console.log("SIGTERM is received");
 	if (server) {
 		server.close();
 	}
