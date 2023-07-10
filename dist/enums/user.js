aa570cd391141b7010893823277ab8f0d4d13a6e
MdTasib <ohidulalam442@gmail.com>
2023-07-10 19:54:08 +0600

    deploy

--- dist/enums/user.js
+++ dist/enums/user.js
@@ -8,4 +8,4 @@ var ENUM_USER_ROLE;
     ENUM_USER_ROLE["ADMIN"] = "admin";
     ENUM_USER_ROLE["SELLER"] = "seller";
     ENUM_USER_ROLE["BUYER"] = "buyer";
-})(ENUM_USER_ROLE || (exports.ENUM_USER_ROLE = ENUM_USER_ROLE = {}));
+})(ENUM_USER_ROLE = exports.ENUM_USER_ROLE || (exports.ENUM_USER_ROLE = {}));
